20347969f2cfd6b665f9036620372f3e83a96af4
guney bilen <guneybilen@yahoo.com>
2022-06-26 22:55:39 -0400

    routes_jobs completed

---
+++ README.md
@@ -0,0 +1,34 @@
+# This Website is based on the assistance provided by the
+
+# great people that created fastapitutorial.com
+
+## Technology Stack:
+
+- FastAPI
+- Uvicorn (server)
+- Pytest
+- Sqlalchemy
+- Postgres
+
+Features:
+
+- ✔️ Course [FastAPI Course](https://www.fastapitutorial.com/blog/fastapi-course/)
+- ✔️ Serving Template
+- ✔️ Static Files in Development
+- ✔️ Connecting to Database
+- ✔️ Schemas
+- ✔️ Dependency Injection
+- ✔️ Password Hashing
+- ✔️ Unit Testing (What makes an app stable)
+- ✔️ Authentication login/create user/get token
+- ✔️ Authorization/Permissions
+- ✔️ Webapp (Monolithic)
+- 🚧 Load Testing using Locust
+- 🚧 Fully Asyc
+- 🚧 Migration by alembic
+- 🚧 Caching
+- 🚧 Dockerization
+- 🚧 Creating a frontend using Vue/React
+- 🚧 Getting ready for Production e.g. load balancing,NGINX,HTTPS
+- 🚧 Deployment
+- 🚧 CI and CD
---
+++ routers/routes/routes_jobs.py
@@ -0,0 +1,87 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.templating import Jinja2Templates
+from db.repository.jobs import _update_job_by_id, _delete_job_by_id, _create_new_job
+from db.repository.jobs import _list_jobs, _retrieve_jobs, _search_jobs
+
+from typing import List
+from typing import Optional
+
+from db.models.users import User
+
+from db.session import get_db
+
+from schemas.jobs import JobCreate, ShowJob
+from sqlalchemy.orm import Session
+
+import route_login as _route_login  
+
+router = APIRouter()
+templates = Jinja2Templates(directory="templates")
+
+@router.post("/create-job", response_model=ShowJob)
+def create_job(job: JobCreate, 
+               db: Session = Depends(get_db), 
+               current_user: User = Depends(get_current_user_from_token)):
+
+               job = create_new_job(job=job, db=db, owner_id=current_user.id)
+               return job
+
+
+# if we keep just "{id}". it would start catching all routes
+@router.get("/get/{id}", response_model=ShowJob)
+def read_job(id: int, db: Session = Depends(get_db)):
+  job = retrieve_job(id=id, db=db)
+  
+  if not job:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+    detail=f"Job with this id {id} does not exist")
+
+  return job
+
+@router.get("/all", response_model=List(ShowJob))
+def read_jobs(db: Session = Depends(get_db)):
+  jobs = list_jobs(db=db)
+  return jobs
+
+@router.put("/update/{id}")
+def update_job(id: int, job: JobCreate, db: Session = Depends(get_db)):
+  current_user = 1
+  message = update_job_by_id(id=id, job=job, db=db owner_id=current_user)
+  
+  if not message:
+    raise HTTPException(
+      status_code=status.HTTP_404_NOT_FOUND,
+      detail=f"Job with id {id} not found"
+    )
+
+  return {"msg": "Successfully updated data."}
+
+
+
+@router.delete_job("/delete/{id}")
+def delete_job(id: int,
+               db : Session = Depends(get_db),
+              current_user: User = Depends(get_current_user_from_token)):
+
+  job = retrieve_job(id=id, db=db)
+  if not job: 
+    return HTTPException(
+      status_code=status.HTTP_404_NOT_FOUND,
+      detail=f"Job with id {id} not found"
+    )
+  print(job.owner_id, current_user.id, current_user.is_superuser)
+  if job.owner_id == current_user.id or current_user.is_superuser:
+    _delete_job_by_id(id=id, db=db, owner=current_user.id)
+    return {"detail": "Successfully deleted"}
+  raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You are not permitted!")
+
+@router.get("/autocomplete")
+def autocomplete(term: Optional[str] = None, db: Session = Depends(get_db)):
+  jobs = search_job(term, db=db)
+  job_titles = []
+  for job in jobs:
+    job_titles.append(job.title)
+  return job_titles
+
+
+
