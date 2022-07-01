cc6da55387eec930140d3155b2657e63f8412e35
guney bilen <guneybilen@yahoo.com>
2022-07-01 17:27:39 -0400

    secret question hashing implemented

--- db/repository/users.py
+++ db/repository/users.py
@@ -2,19 +2,19 @@ from core.hashing import Hasher
 from ..models.users import User    
 from schemas.users import UserCreate 
 from sqlalchemy.orm import Session  
-
+from passlib.hash import bcrypt
 
 def create_new_user(user: UserCreate, db: Session):
   user_being_saved = User(
         username=user.username,
         email=user.email,
-        hashed_password=Hasher.get_password_hash(user.password),
+        hashed_password=Hasher.get_hash(user.password),
         is_active=True,
         is_superuser=False,
         first_name=user.first_name,
         last_name=user.last_name,
         s_name=user.s_name,
-        s_answer=user.s_answer
+        s_answer=Hasher.get_hash(user.s_answer)
   )
   db.add(user_being_saved)
   db.commit() 
